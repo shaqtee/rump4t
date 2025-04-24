2d32ffc8a924496b528d2039d9d0828928de25b0
ABDULBADI\HP <adi.sakti@live.com>
2025-04-24 16:48:34 +0700

    updates

--- Modules/Masters/App/Models/MasterDistrict.php
+++ Modules/Masters/App/Models/MasterDistrict.php
@@ -14,5 +14,43 @@ class MasterDistrict extends Model
      * The attributes that are mass assignable.
      */
     protected $table = "m_districts";
-    protected $guarded = ['id'];    
+    protected $guarded = ['id'];
+
+    public function scopeFilter($query, $request)
+    {
+        // if ($request->has('search')) {
+        //     $key = $request->search;
+        // }
+
+        foreach ($request->all() as $key => $val) {
+            if ($key === 'search' || $key === 'size' || $key === 'page') {
+            } else {
+                if ($request->has($key)) {
+                    if ($val !== null) {
+                        switch ($key) {
+                            case 'code':
+                                $query->where('code', 'ilike', '%' . $val . '%');
+                                break;
+                            case 'name':
+                                $query->where('name', 'ilike', '%' . $val . '%');
+                                break;
+                            default:
+                                $query->where($key, 'ilike', '%' . $val . '%');
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        return $query;
+    }
+
+    public static function columns()
+    {
+        return Helper::columns([
+            'Code' => 'string',
+            'Name' => 'string',
+        ]);
+    }
 }
