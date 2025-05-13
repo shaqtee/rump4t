e2619093eb5b4920ff92201a8bd840ff24b30503
ABDULBADI\HP <adi.sakti@live.com>
2025-05-13 09:28:45 +0700

    updates R4-55
    - sembunyikan sidebar menu admin pada role pengurus
    - bug minor dropdown profile tidak muncul
    - pic profile jika belum save photo
    - pengurus -> data profil anggota pada region yang sama
    - pengurus -> posting sosmed sesuai region anggota
    - pengurus -> posting berita khusus untuk region author
    - pengurus -> posting kegiatan untuk region masing - masing

--- resources/views/Admin/Layouts/sidebar.blade.php
+++ resources/views/Admin/Layouts/sidebar.blade.php
@@ -15,7 +15,7 @@
             <div class="app-sidebar__user clearfix">
                 <div class="dropdown user-pro-body">
                     <div class="">
-                        <img alt="user-img" class="avatar avatar-xl brround" src="{{ Auth::user()->image }}">
+                        <img alt="user-img" class="avatar avatar-xl brround" src="{{ Auth::user()->image ?? 'https://placehold.co/400x400?text=No+Image'}}">
                         <span class="avatar-status profile-status bg-green"></span>
                     </div>
                     <div class="user-info">
@@ -133,9 +133,17 @@
                         </li> --}}
                     </ul>
                 </li>
+                @if(auth()->user()->t_group_id != 3)
                 <li class="slide">
-                    <a class="side-menu__item" href="{{ route('users.admin.semua') }}"><svg xmlns="http://www.w3.org/2000/svg" class="side-menu__icon" viewBox="0 0 24 24" ><path d="M0 0h24v24H0V0z" fill="none"/><path d="M5 9h14V5H5v4zm2-3.5c.83 0 1.5.67 1.5 1.5S7.83 8.5 7 8.5 5.5 7.83 5.5 7 6.17 5.5 7 5.5zM5 19h14v-4H5v4zm2-3.5c.83 0 1.5.67 1.5 1.5s-.67 1.5-1.5 1.5-1.5-.67-1.5-1.5.67-1.5 1.5-1.5z" opacity=".3"/><path d="M20 13H4c-.55 0-1 .45-1 1v6c0 .55.45 1 1 1h16c.55 0 1-.45 1-1v-6c0-.55-.45-1-1-1zm-1 6H5v-4h14v4zm-12-.5c.83 0 1.5-.67 1.5-1.5s-.67-1.5-1.5-1.5-1.5.67-1.5 1.5.67 1.5 1.5 1.5zM20 3H4c-.55 0-1 .45-1 1v6c0 .55.45 1 1 1h16c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1zm-1 6H5V5h14v4zM7 8.5c.83 0 1.5-.67 1.5-1.5S7.83 5.5 7 5.5 5.5 6.17 5.5 7 6.17 8.5 7 8.5z"/></svg><span class="side-menu__label">Admin</span></a>
+                    <a class="side-menu__item" href="{{ route('users.admin.semua') }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="side-menu__icon" viewBox="0 0 24 24" >
+                            <path d="M0 0h24v24H0V0z" fill="none"/><path d="M5 9h14V5H5v4zm2-3.5c.83 0 1.5.67 1.5 1.5S7.83 8.5 7 8.5 5.5 7.83 5.5 7 6.17 5.5 7 5.5zM5 19h14v-4H5v4zm2-3.5c.83 0 1.5.67 1.5 1.5s-.67 1.5-1.5 1.5-1.5-.67-1.5-1.5.67-1.5 1.5-1.5z" opacity=".3"/>
+                            <path d="M20 13H4c-.55 0-1 .45-1 1v6c0 .55.45 1 1 1h16c.55 0 1-.45 1-1v-6c0-.55-.45-1-1-1zm-1 6H5v-4h14v4zm-12-.5c.83 0 1.5-.67 1.5-1.5s-.67-1.5-1.5-1.5-1.5.67-1.5 1.5.67 1.5 1.5 1.5zM20 3H4c-.55 0-1 .45-1 1v6c0 .55.45 1 1 1h16c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1zm-1 6H5V5h14v4zM7 8.5c.83 0 1.5-.67 1.5-1.5S7.83 5.5 7 5.5 5.5 6.17 5.5 7 6.17 8.5 7 8.5z"/>
+                        </svg>
+                        <span class="side-menu__label">Admin</span>
+                    </a>
                 </li>
+                @endif
                 {{-- <li class="slide"> --}}
                     {{-- <a class="side-menu__item" href="{{ route('letsplay.semua') }}"><svg xmlns="http://www.w3.org/2000/svg" class="side-menu__icon" viewBox="0 0 24 24" ><path d="M0 0h24v24H0V0z" fill="none"/><path d="M5 9h14V5H5v4zm2-3.5c.83 0 1.5.67 1.5 1.5S7.83 8.5 7 8.5 5.5 7.83 5.5 7 6.17 5.5 7 5.5zM5 19h14v-4H5v4zm2-3.5c.83 0 1.5.67 1.5 1.5s-.67 1.5-1.5 1.5-1.5-.67-1.5-1.5.67-1.5 1.5-1.5z" opacity=".3"/><path d="M20 13H4c-.55 0-1 .45-1 1v6c0 .55.45 1 1 1h16c.55 0 1-.45 1-1v-6c0-.55-.45-1-1-1zm-1 6H5v-4h14v4zm-12-.5c.83 0 1.5-.67 1.5-1.5s-.67-1.5-1.5-1.5-1.5.67-1.5 1.5.67 1.5 1.5 1.5zM20 3H4c-.55 0-1 .45-1 1v6c0 .55.45 1 1 1h16c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1zm-1 6H5V5h14v4zM7 8.5c.83 0 1.5-.67 1.5-1.5S7.83 5.5 7 5.5 5.5 6.17 5.5 7 6.17 8.5 7 8.5z"/></svg><span class="side-menu__label">Let's Play management</span></a> --}}
                     {{-- <ul class="slide-menu">
