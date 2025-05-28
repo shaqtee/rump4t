486d9796704f443f271bf9a5911f2f2e353ccaee
Devi Puspita P <devi.puspita.pribadi.xtkj2@gmail.com>
2025-05-28 09:53:41 +0700

    updateee

---
+++ Modules/Polling/Database/Seeders/PollingDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Polling\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class PollingDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
