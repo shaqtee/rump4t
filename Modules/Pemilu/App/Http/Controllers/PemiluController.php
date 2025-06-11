7710a52d9af98d413d18498d778061027c278032
ABDULBADI\HP <adi.sakti@live.com>
2025-06-11 09:01:15 +0700

    Pemilu api

--- Modules/Pemilu/App/Http/Controllers/PemiluController.php
+++ Modules/Pemilu/App/Http/Controllers/PemiluController.php
@@ -2,19 +2,47 @@
 
 namespace Modules\Pemilu\App\Http\Controllers;
 
+use DB;
+use Modules\Pemilu\App\Http\Requests\PemiluRequest;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\RedirectResponse;
+use App\Models\Pemilu;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+
 
 class PemiluController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request): JsonResponse
     {
-        return view('pemilu::index');
+        try {
+            $query = Pemilu::query();
+            $page = $request->input('size', 10);
+            
+            if ($request->has('size')) {
+                $pemilus = $query
+                    ->filter($request)
+                    ->paginate($page)
+                    ->appends($request->all());
+            }else{
+                $pemilus = $query->filter($request)->get();
+            }
+
+            return response()->json([
+                'status' => 'success',
+                'data' => $pemilus
+            ]);
+        } catch (\Exception $e) {
+            report($e);
+
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'Terjadi kesalahan saat mengambil data.',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 
     /**
@@ -28,9 +56,32 @@ class PemiluController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request): RedirectResponse
+    public function store(PemiluRequest $request): JsonResponse
     {
-        //
+        try {
+            DB::beginTransaction();
+    
+            $data = $request->validated();
+            $data['created_by'] = auth()->id();
+                
+            $pemilu = Pemilu::create($data);
+    
+            DB::commit();
+    
+            return response()->json([
+                "status" => "success",
+                "data" => $pemilu
+            ], 201);
+
+        } catch (\Exception $e) {
+            report($e);
+            DB::rollback();
+
+            return response()->json([
+                "status" => "failed",
+                "error" => $e->getMessage()
+            ], 500);
+        }
     }
 
     /**
@@ -52,9 +103,34 @@ class PemiluController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, $id): RedirectResponse
+    public function update(Request $request, Pemilu $pemilu): JsonResponse
     {
-        //
+        try {
+            DB::beginTransaction();
+            $data = $request->validate([
+                'title' => 'sometimes|required|string|max:255',
+                'description' => 'sometimes|required|string',
+                'start_date' => 'sometimes|required|date',
+                'end_date' => 'sometimes|required|date',
+                'is_active' => 'sometimes|required|bool',
+            ]);
+
+            $pemilu->update($data);
+            DB::commit();
+
+            return response()->json([
+                "status" => "success",
+                "data" => $pemilu
+            ], 201);
+        } catch (\Exception $e) {
+            report($e);
+            DB::rollback();
+
+            return response()->json([
+                "status" => "failed",
+                "error" => $e->getMessage()
+            ], 500);
+        }
     }
 
     /**
@@ -62,6 +138,26 @@ class PemiluController extends Controller
      */
     public function destroy($id)
     {
-        //
+        try {
+            DB::beginTransaction();
+            $pemilu = Pemilu::findOrFail($id);
+            $pemilu->delete();
+            DB::commit();
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Pemilu berhasil dihapus.'
+            ]);
+
+        } catch (\Exception $e) {
+            report($e);
+            DB::rollback();
+
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'Terjadi kesalahan saat menghapus data.',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 }
--- Modules/Pemilu/routes/api.php
+++ Modules/Pemilu/routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Modules\Pemilu\App\Http\Controllers\PemiluController;
+use Modules\Pemilu\App\Http\Controllers\CandidateController;
+use Modules\Pemilu\App\Http\Controllers\PollingsController;
 
 /*
     |--------------------------------------------------------------------------
@@ -14,6 +17,29 @@ use Illuminate\Support\Facades\Route;
     |
 */
 
-Route::middleware(['auth:sanctum'])->prefix('v1')->name('api.')->group(function () {
-    Route::get('pemilu', fn (Request $request) => $request->user())->name('pemilu');
+Route::middleware('auth:api')->group(function () {
+    Route::prefix('pemilu')->group(function() {
+        /* CRUD */
+        Route::get('/',[PemiluController::class, 'index']);
+        Route::post('store',[PemiluController::class, 'store']);
+        Route::patch('update/{pemilu}',[PemiluController::class, 'update']);
+        Route::delete('delete/{id}',[PemiluController::class, 'destroy']);
+
+        /* Kandidat */
+        Route::prefix('candidate')->group(function() {
+            Route::get('{pemilu_id}', [CandidateController::class ,'index']);
+            Route::post('add', [CandidateController::class ,'add']);
+            Route::delete('{candidate}/left', [CandidateController::class ,'left']);
+            Route::patch('{candidate}/activate', [CandidateController::class ,'activate']);
+        });
+
+        /* Pollings */
+        Route::prefix('pollings')->group(function() {
+            Route::get('/', [PollingsController::class ,'index']);
+            Route::get('pre-vote-users', [PollingsController::class ,'pre_vote_users']);
+            Route::post('votes', [PollingsController::class ,'votes']);
+            Route::get('index-voted/{pemilu_id}', [PollingsController::class ,'index_voted']);
+            Route::delete('cancel-voted/{voted_id}', [PollingsController::class ,'cancel_voted']);
+        });
+    });
 });
