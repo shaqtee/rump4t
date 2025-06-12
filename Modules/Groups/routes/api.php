368ce1caf7b066a6693a87a945fd4fd0d3f036b8
ABDULBADI\HP <adi.sakti@live.com>
2025-06-12 15:18:27 +0700

    update
    - member group: index, add, left, be admin

--- Modules/Groups/routes/api.php
+++ Modules/Groups/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Modules\Groups\App\Http\Controllers\GroupsController;
+use Modules\Groups\App\Http\Controllers\PostingController;
 
 /*
     |--------------------------------------------------------------------------
@@ -22,5 +23,18 @@ Route::middleware('auth:api')->group(function () {
         Route::post('store',[GroupsController::class, 'store']);
         Route::post('update/{id}',[GroupsController::class, 'update']);
         Route::delete('delete/{id}',[GroupsController::class, 'destroy']);
+
+        Route::prefix('member')->group(function() {
+            /* Member */
+            Route::get('/{group_id}',[GroupsController::class, 'index_member']);
+            Route::post('add',[GroupsController::class, 'add_member']);
+            Route::delete('left',[GroupsController::class, 'left_member']);
+            Route::patch('be-admin',[GroupsController::class, 'be_admin']);
+        });
+
+        Route::prefix('posting')->group(function() {
+            /* Posting */
+            Route::get('/{group_id}',[PostingController::class, 'index_posting']);
+        });
     });
 });
