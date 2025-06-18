3e9992a848f5403a954d57b9deab8c038da0ddc2
DESKTOP-FNCDK6J\user <adi.sakti@live.com>
2025-06-18 17:45:34 +0700

    eVoucher jadi EVoucher

--- Modules/Community/App/Emails/EVoucher.php
+++ Modules/Community/App/Emails/EVoucher.php
@@ -7,7 +7,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class eVoucher extends Mailable
+class EVoucher extends Mailable
 {
     use Queueable, SerializesModels;
 
